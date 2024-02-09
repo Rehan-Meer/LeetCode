39d7f8ecca6ceab0e8363766a0bbea2228659537
Rayhan-Meer <rehanmeer38@gmail.com>
2024-02-09 22:33:08 +0500

    Isomorphic String Commit

--- LeetCodeSolver.cs
+++ LeetCodeSolver.cs
@@ -1,10 +1,41 @@
-﻿namespace LeetCode
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
 {
     public static class LeetCodeSolver
     {
+        public static bool IsIsomorphicString(string source, string target)
+        {
+            var dict = new Dictionary<char, char>();
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (dict.ContainsKey(source[i]))
+                {
+                    dict.TryGetValue(source[i], out char val);
+                    if (val != target[i])
+                        return false;
+                    else
+                        continue;
+                }
+
+                if (dict.ContainsValue(target[i]))
+                {
+                    if (dict.FirstOrDefault(x => x.Value == source[i]).Key != target[i])
+                        return false;
+                    else
+                        continue;
+                }
+                else
+                    dict.Add(source[i], target[i]);
+            }
+            return true;
+        }
+
         static void Main()
         {
-            // Method intentionally left empty.
+           Console.WriteLine(LeetCodeSolver.IsIsomorphicString("add", "egg"));
         }
     }
 }
