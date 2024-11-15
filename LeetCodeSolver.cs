78f38d7d2558aab8242d16648b4ee4a6958ffd67
Rayhan-Meer <rehanmeer38@gmail.com>
2024-11-16 00:55:07 +0500

    LeetCode : LengthOfLongestSubstring

--- LeetCodeSolver.cs
+++ LeetCodeSolver.cs
@@ -133,34 +133,88 @@ namespace LeetCode
         }
         public static int[] TwoSum(int[] nums, int target)
         {
-
             int firstIndex = -1;
             var len = nums.Length;
-
             for (int i = 0; i < len; i++)
             {
-
                 if (firstIndex == -1)
                     firstIndex = i;
-
                 if (i == len - 1 && nums[i] + nums[firstIndex] != target)
                 {
                     i = firstIndex;
                     firstIndex = -1;
                     continue;
                 }
-
                 if (i != firstIndex && nums[i] + nums[firstIndex] == target)
                     return new int[] { firstIndex, i };
-
             }
             return new int[] { };
         }
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var numList = nums.ToList();
+            int ArrLength = nums.Length;
+            int[] Output = new int[ArrLength];
+            for (int iterateNo = 0; iterateNo < ArrLength; iterateNo++)
+            {
+                Output[iterateNo] = numList.Select((value, index) => new { Value = value, Index = index })
+                                    .Where(x => x.Index != iterateNo)
+                                    .Select(x => x.Value)
+                                    .Aggregate(1, (acc, val) => acc * val);
+            }
+            return Output;
+        }
+        public static string MergeAlternately(string word1, string word2)
+        {
+            var word1List = word1.ToCharArray();
+            var word2List = word2.ToCharArray();
+            int word1Length = word1List.Length;
+            int word2Length = word2List.Length;
+            string output = String.Empty;
+            for (var index = 0; index < word1Length; index++)
+            {
+                if (word1Length > word2Length)
+                {
+                    output += word1List[index];
+                    if (index < word2Length)
+                        output += word2List[index];
+                }
+                else
+                {
+                    output += word1List[index];
+                    output += word2List[index];
+                    if (index + 1 == word1Length)
+                        output += (new string(word2List.Skip(index + 1).Take(word2Length - (index + 1)).ToArray()));
+                }
+            }
+            return output;
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length <= 1)
+            { return s.Length; }
+            char[] charArr = s.ToCharArray();
+            List<string> combo = new List<string>();
+            Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
 
-
+            foreach (char c in charArr)
+            {
+                if (!keyValuePairs.ContainsKey(c))
+                    keyValuePairs.Add(c, 1);
+                else
+                {
+                    combo.Add(string.Join("", keyValuePairs.Select(kv => kv.Key)));
+                    keyValuePairs.Clear();
+                    keyValuePairs.Add(c, 1);
+                }
+            }
+            if (!combo.Any())
+                combo.Add(string.Join("", keyValuePairs.Select(kv => kv.Key)));
+            return combo.Any() ? combo.OrderByDescending(d => d.Length).First().Length : 0;
+        }
         static void Main()
         {
-            Console.WriteLine(LeetCodeSolver.IsIsomorphicString("add", "egg"));
+            //Call the desired method here.
         }
     }
 }
